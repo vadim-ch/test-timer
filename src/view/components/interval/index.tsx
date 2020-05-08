290a2335523c4b19277f5f25de02b5cbd85a2481
Vadim <vchistokhvalov@ronte.io>
2020-05-08 14:10:01 +0300

    test 10

--- src/view/components/interval/index.tsx
+++ src/view/components/interval/index.tsx
@@ -14,7 +14,7 @@ const Interval: React.FC<IIntervalProps> = ({ currentInterval, increaseInterval,
   return (
     <IntervalBox>
       <IntervalTitle>Интервал обновления секундомера: <IntervalValue>{currentInterval}</IntervalValue> сек.</IntervalTitle>
-      Test 9
+      Test 10
       <ButtonGroup>
          <Button color="blue" disabled={!isDecAvailable} onClick={isDecAvailable ? decreaseInterval : undefined}>-</Button>
          <Button color="blue" onClick={increaseInterval}>+</Button>
